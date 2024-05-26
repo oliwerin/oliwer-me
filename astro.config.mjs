b1d98521efa364614097b18b300785258c2d2573
Oliwer iwanicki <oliwer.iwanicki@gmail.com>
2024-05-26 15:33:49 +0200

    Tutorial unit 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://oliwerin.github.io",
+  site: "https://oliwer-me.pages.dev",
 });
