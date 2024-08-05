7a41131d1fa2446eb8ae5615fe5caec0d2325663
Oliwer iwanicki <oliwer.iwanicki@gmail.com>
2024-08-05 22:26:11 +0200

    Make snake faster

--- src/components/SnakeGame/constants/constants.tsx
+++ src/components/SnakeGame/constants/constants.tsx
@@ -1,6 +1,6 @@
 import { SnakeDirection } from "../types/types";
 
-export const REFRESH_RATE_MS = 500;
+export const REFRESH_RATE_MS = 250;
 export const GAME_AREA_SIZE = 20;
 export const SNAKE_STARTING_POSITION = {
   x: 10,
