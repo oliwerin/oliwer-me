a2bd0294c4cd1f364c3c3ba9908b46a7d54a2e69
Oliwer iwanicki <oliwer.iwanicki@gmail.com>
2024-08-06 23:00:07 +0200

    Add the text-shadow support

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      textShadow: {
+        DEFAULT: "1px 1px 2px transparent",
+        dark: "1px 1px 2px rgb(57, 62, 70)",
+      },
+    },
     colors: {
       black: "rgb(57, 62, 70)",
       white: "rgb(247, 247, 247)",
@@ -15,5 +22,16 @@ export default {
     },
   },
   darkMode: ["selector", "html.dark"],
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") },
+      );
+    }),
+  ],
 };
